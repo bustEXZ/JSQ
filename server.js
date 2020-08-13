56069e665079bc0d2b0c0311e648aa0bc09cf946
aio350 <aio350@yahoo.com>
2020-08-13 15:38:09 +0500

    remove

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')()
-
-app.use((req, res) => {
-    res.setHeader('Accept-Encoding', 'gzip, compress, br')
-    res.sendFile(__dirname + decodeURIComponent(req.url))
-})
-
-app.listen(process.env.PORT || 3000)
