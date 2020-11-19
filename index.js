507aefd8fffcaf16614f23b33ad07e9b7b86da20
Igor Agapov <aio350@yahoo.com>
2020-11-19 22:05:04 +0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const app = require("express")();
+const app = require('express')()
 
 app.use((req, res) => {
-  res.setHeader("Accept-Encoding", "gzip, compress, br");
-  res.sendFile(__dirname + decodeURIComponent(req.url));
-});
+  res.setHeader('X-Frame-Options', 'sameorigin')
+  res.setHeader('Content-Security-Policy', "frame-ancestors 'self';")
+  res.setHeader('Accept-Encoding', 'gzip, compress, br')
+  res.sendFile(__dirname + decodeURIComponent(req.url))
+})
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
