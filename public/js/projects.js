378f1ea21842df708e670f78f4191ec2dcc08f50
Igor Agapov <aio350@yahoo.com>
2020-12-25 08:53:09 +0500

    update

--- public/js/projects.js
+++ public/js/projects.js
@@ -3,13 +3,10 @@ export default /*html*/ `
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Projects">Репозиторий с проектами</a>
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples">Репозиторий с примерами</a>
   <h3>Проекты</h3>
-  <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/ZEEPRba">Визуальный эффект из фильма "Матрица" (Canvas API)</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/eYmGwJK">Генератор галлереи изображений со встроенным слайдером (File API)</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/abzKLvM">Визуализация аудиоданных (Web Audio API, Canvas API)</a>
-  <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/mdJVErG">Анимированное сердце (Canvas API)</a>
-  <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/rNVEGya">Pixel Art Maker (Canvas API)</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/wvKWGVL">Приложение для медитации</a>
-  <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/oNjLLXd">Приложение для погоды</a>
+  <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/oNjLLXd">Приложение для показа погоды</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/wvKeezJ">Виртуальная клавиатура</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/wvKryPO">Простой калькулятор (Math.js)</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/jObGgqY">Игра "Камень, ножницы, бумага"</a>
@@ -17,8 +14,6 @@ export default /*html*/ `
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/gOaXYQZ">Игра "Пинг-понг"</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/gOaXYQZ">Приложение для хранения списка задач (IndexedDB)</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/LYpdJOB">Игра "Тетрис"</a>
-  <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/YzyLEzW">Интересный эффект слежения за курсором (Canvas API)</a>
-  <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/MWaGOYZ">Буквы, разрушающиеся при движении курсора (Canvas API)</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/xxwyqqx">Приложение для конвертации валюты</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/WNQapMK">Игра "Виселица"</a>
   <a target="_blank" rel="noopener" href="https://codepen.io/harryheman/pen/abvRJjO">Приложение для поиска текстов песен</a>
@@ -38,17 +33,16 @@ export default /*html*/ `
 </section>
 <section>
   <h3>Примеры</h3>
+  <a target="_blank" rel="noopener" href="https://habr.com/ru/post/532902/">Примеры работы с картой</a>
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Drag-n-Drop">Пример использования Dran'n'Drop API</a>
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Intersection-Observer">Пример использования ES6-модулей и Canvas API</a>
-  <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Parcel">Пример использования Parcel для сборки проекта</a>
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Server-Sent-Events">Пример использования Server-Sent Events</a>
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Web-Cryptography">Пример использования Web Cryptography API</a>
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Web-Storage">Примеры использования Web Storage API</a>
-  <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Work-With-Files">Примеры работы с файлами в JavaScript</a>
-  <a target="_blank" rel="noopener" href="https://github.com/harryheman/Webpack5-Boilerplate">Пример использования Webpack для сборки проекта</a>
+  <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Examples/tree/main/Work-With-Files">Примеры работы с файлами</a>
   <a target="_blank" rel="noopener" href="https://github.com/harryheman/Client-Side-Rendering">Пример рендеринга страниц на стороне клиента</a>
-  <a target="_blank" rel="noopener" href="https://habr.com/ru/post/525536/">Примеры использования наблюдателей в JavaScript</a>
-  <a target="_blank" rel="noopener" href="https://habr.com/ru/post/500530/">Примеры использования Canvas API</a>
-  <a target="_blank" rel="noopener" href="https://habr.com/ru/post/487962/">Примеры работы с двумерной графикой в JavaScript</a>
+  <a target="_blank" rel="noopener" href="https://habr.com/ru/post/525536/">Примеры использования наблюдателей</a>
+  <a target="_blank" rel="noopener" href="https://habr.com/ru/post/500530/">Примеры работы с Canvas API</a>
+  <a target="_blank" rel="noopener" href="https://habr.com/ru/post/487962/">Примеры работы с двумерной графикой</a>
 </section>
 `
