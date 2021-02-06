cc5f41c5ea84a839dcf18f10380066d489c612ee
Igor Agapov <aio350@yahoo.com>
2021-02-06 11:04:56 +0500

    update

--- public/js/links.js
+++ public/js/links.js
@@ -1,6 +1,19 @@
 export default /*html*/ `
 <section>
-  <h3>Проверенные источники</h3>
+  <h3>Другие проекты</h3>
+  <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Projects">Примеры клиентских и серверных (клиент-серверных) приложений на JavaScript</a>
+  <a target="_blank" rel="noopener" href="https://github.com/harryheman/JavaScript-Design-Patterns">Паттерны проектирования на JavaScript</a>
+  <a target="_blank" rel="noopener" href="https://nodejs-guide.ru">Руководство по Node.js для начинающих</a>
+  <a target="_blank" rel="noopener" href="https://github.com/harryheman/React-Total">Тотальный React</a>
+  <a target="_blank" rel="noopener" href="https://htmltemplate.site/">Современный стартовый HTML-шаблон</a>
+</section>
+<section class="center">
+  <h3>Контакты</h3>
+  <a target="_blank" rel="noopener" href="https://github.com/harryheman">Мой профиль на GitHub</a>
+  <a target="_blank" rel="noopener" href="https://habr.com/ru/users/aio350/posts/">Мой блог на Хабре</a>
+</section>
+<section>
+  <h3>Источники</h3>
   <a target="_blank" rel="noopener" href="https://thebestcode.ru/public/books/definitive_guide.pdf">"JavaScript. Подробное руководство". Дэвид Флэнаган</a>
   <a target="_blank" rel="noopener" href="https://github.com/azat-io/you-dont-know-js-ru">"Вы не знаете JS". Кайл Симпсон</a>
   <a target="_blank" rel="noopener" href="https://learn.javascript.ru/">"Современный учебник JavaScript". Илья Кантор</a>
@@ -15,5 +28,6 @@ export default /*html*/ `
   <a target="_blank" rel="noopener" href="https://github.com/lydiahallie/javascript-questions">JavaScript Questions</a>
   <a target="_blank" rel="noopener" href="https://github.com/sadanandpai/javascript-code-challenges">JavaScript Modern Interview Code Challenges</a>
   <a target="_blank" rel="noopener" href="https://github.com/yeungon/In-JavaScript-we-trust">In JavaScript We Trust</a>
+  <a target="_blank" rel="noopener" href="https://edabit.com/challenges">Edabit - практические задания по JavaScript</a>
 </section>
 `
